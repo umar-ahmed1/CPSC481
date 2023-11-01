129da7325191f1cbe4f5d0fdb0cbad7b61868a2b
Aarij Ashar <arij.ashar02@gmail.com>
2023-11-01 01:37:15 -0600

    partial day planner

---
+++ scripts/add.js
@@ -0,0 +1,10 @@
+function addMenu() {
+    var popup = document.getElementById('add-menu');
+    
+    popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
+}
+function closeAddMenu() {
+    var popup = document.getElementById('add-menu');
+    
+    popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
+}
