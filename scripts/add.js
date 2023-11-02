3f15962a5a9538f3a01fa14866998f99ff18662a
Aarij Ashar <arij.ashar02@gmail.com>
2023-11-01 18:58:22 -0600

    complete day planner

--- scripts/add.js
+++ scripts/add.js
@@ -8,3 +8,29 @@ function closeAddMenu() {
     
     popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
 }
+
+function increment() {
+    var numberField = document.getElementById('numberField');
+    var currentValue = parseInt(numberField.value, 10);
+    if (currentValue < parseInt(numberField.max, 10)) {
+        numberField.value = currentValue + 1;
+    }
+}
+
+function decrement() {
+    var numberField = document.getElementById('numberField');
+    var currentValue = parseInt(numberField.value, 10);
+    if (currentValue > parseInt(numberField.min, 10)) {
+        numberField.value = currentValue - 1;
+    }
+}
+
+function change(button) {
+    if (button.textContent === 'ADD') {
+        button.style.width = '80px';
+        button.textContent = 'REMOVE';
+    } else {
+        button.textContent = 'ADD';
+        button.style.width = '50px';
+    }
+}
