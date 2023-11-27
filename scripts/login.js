7b2910acc88ab5b29649b0a75e32fdc236a3f79f
Umar Ahmed <108314943+umar-ahmed1@users.noreply.github.com>
2023-11-26 19:30:57 -0700

    added user login functionality

---
+++ scripts/login.js
@@ -0,0 +1,27 @@
+// Function to handle user login
+function loginUser(event) {
+    event.preventDefault();
+
+    // Get login form details
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Get all existing users from local storage
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+
+    // Check if the entered credentials match any user in the list of users
+    const user = users.find(u => u.email === email && u.password === password);
+
+    //If user details correct, store the logged in user and redirect to landing page
+    if (user) {
+        localStorage.setItem('loggedInUser', JSON.stringify(user));
+        window.location.href = 'landing.html';
+    // Display an error message
+    } else {
+        alert('Invalid email or password. Please try again.');
+    }
+}
+
+// Attach event listener to the form
+const loginForm = document.querySelector('.reg-form');
+loginForm.addEventListener('submit', loginUser);
