39f5f503271b4fb0a0ef0e07d06403703f0e26ce
JVanderstoop <J.vanderstoop@outlook.com>
2023-11-27 19:32:32 -0700

    most of the map functionality is complete

--- scripts/attractions.js
+++ scripts/attractions.js
@@ -1,5 +1,5 @@
 function redirect(param) {
     const attractionsHome = 'http://127.0.0.1:5500/pages/attractions-home.html'
     let search = "?attraction=" + param
-    window.location.assign(attractionsHome + search);
+    window.open(attractionsHome + search, '_blank');
 }
--- scripts/mapPage.js
+++ scripts/mapPage.js
@@ -11,25 +11,74 @@ for (i = 0; i < toggler.length; i++) {
 toggler = document.getElementsByClassName("unchecked");
 for (i = 0; i < toggler.length; i++) {
   toggler[i].addEventListener("click", function() {
-    this.classList.toggle("checked")
+    if (this.classList.contains("checked")) {
+      this.classList.remove("checked"); 
+      this.classList.add("unchecked"); 
+    } else if (this.classList.contains("unchecked")) {
+      this.classList.add("checked"); 
+      this.classList.remove("unchecked"); 
+    }
+    highlightBox = document.getElementById(this.id.replace('Selector','')); 
+    console.log(highlightBox.style.display);
+    if (highlightBox.style.display === "block") {
+      highlightBox.style.display = "none";
+    } else {
+      highlightBox.style.display = "block";
+    }
   });
 }
 
-document.getElementById("start-concerts-expanded").click();
-document.getElementById("start-nashville-north-clicked").click();
-
-function zoom(zoom) {
-  const x_modify = 100 * zoom;
-  const y_modify = 178 * zoom;
-  let map = document.getElementById("map");
+function zoom(zoomFactor) {
+  let mapImage = document.getElementById("mapImage");
+  let map = mapImage.getBoundingClientRect();
   let mapDiv = document.getElementById("mapContainer");
   let mapContainer = mapDiv.getBoundingClientRect();
 
-  // Do not zoom out any more if the map dimensions are smaller than the maps container - 
-  // disallows white space on the page
-  if (((map.width + x_modify) > mapContainer.width) && ((map.height + y_modify) > mapContainer.height)){
-    map.width += x_modify;
-    map.height += y_modify;
-  }
+  const x_modify = (map.width * 0.069) * zoomFactor;
+  const y_modify = (map.height * 0.046) * zoomFactor;
+
+  let newWidth = map.width + x_modify;
+  let newHeight = map.height + y_modify;
+
+  // Check if new dimensions are within the limits
+  if (newWidth >= mapContainer.width && newHeight >= mapContainer.height) {
+      mapImage.style.width = newWidth + "px";
+      mapImage.style.height = newHeight + "px";
+      mapImage.style.backgroundSize = `${newWidth}px ${newHeight}px`;
+    }
 }
+
+function redirect(param) {
+  const attractionsHome = 'http://127.0.0.1:5500/pages/attractions-home.html'
+  let search = "?attraction=" + param
+  window.open(attractionsHome + search, '_blank');
+}
+
+function toggleVisibility(display) {
+  monsterEnergy = document.getElementById("monsterEnergy");
+  monsterEnergy.style.display = display;
+
+  zipper = document.getElementById("zipper");
+  zipper.style.display = display;
+
+  nashvilleNorth = document.getElementById("nashvilleNorth");
+  nashvilleNorth.style.display = display;
+
+  showHome = document.getElementById("showHome");
+  showHome.style.display = display;
+
+
+
+  selector = document.getElementsByClassName("checkbox"); 
+  for (i = 0; i < selector.length; i++) {
+    if (display === "block") {
+      selector[i].classList.add("checked"); 
+      selector[i].classList.remove("unchecked"); 
+    } else if (display === "none") {
+      selector[i].classList.add("unchecked"); 
+      selector[i].classList.remove("checked"); 
+    }
+  }  
+}
+
 
