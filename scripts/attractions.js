74d558da4af6e25013f9e50a3da52a767e0a10a3
Umar Ahmed <108314943+umar-ahmed1@users.noreply.github.com>
2023-12-02 23:25:40 -0700

    added search bar functionality and more design changes

--- scripts/attractions.js
+++ scripts/attractions.js
@@ -1,5 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //add event listener to the search input
+    const searchInput = document.getElementById('search');
+    searchInput.addEventListener('input', function () {
+        const searchTerm = searchInput.value.toLowerCase();
+        filterAttractions(searchTerm);
+    });
+
+    function filterAttractions(searchTerm) {
+        const attractions = document.querySelectorAll('.grid-item');
+        attractions.forEach(function (attraction) {
+            const attractionId = attraction.id.toLowerCase();
+            const isVisible = attractionId.includes(searchTerm);
+            attraction.style.display = isVisible ? 'flex' : 'none';
+        });
+    }
+});
+
 function redirect(param) {
-    const attractionsHome = 'attractions-home.html'
+    const attractionsHome = 'attractions-home.html';
     let search = "?attraction=" + param;
-    window.location.href = attractionsHome + search, '_blank'
+    window.location.href = attractionsHome + search;
 }
